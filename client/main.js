9416ee6cfa86cb98a2804e8277dc03f312de3dba
Rhikshitha Kannan <rhikshithakannan@gmail.com>
2022-09-21 13:12:30 +0530

    Final Push

--- client/main.js
+++ client/main.js
@@ -1,1 +1,48 @@
-// YOUR JAVASCRIPT CODE FOR INDEX.HTML GOES HERE
+const { application } = require("express");
+// This function runs when hospital sends request for access 
+function postHospitalDetails(){
+    
+    var hospitalname=document.getElementById("hospitalnameId").value;
+    var hospitalcontact=document.getElementById("contactnumberId").value;
+    var emailId = document.getElementById("emailId").value;
+    var address = document.getElementById("addressId").value;
+    console.log(hospitalname);
+
+    $.ajax({
+        url:"/server/unified_patientportal_function/hrequest",
+        type:"post",
+        contentType:"application/json",
+        data: JSON.stringify({
+            "hospitalName":hospitalname,
+            "contactNumber":hospitalcontact,
+            "mailId":emailId,
+            "address":address
+        }),
+        success: function(data){
+            alert(data.message);
+        },
+        error: function(error){
+            alert(error.message);
+        }
+    });
+}
+
+// Login hospital
+function HospitalLogin(){
+   var hid;
+   var hpass;
+   $.ajax({
+    url:"/server/unified_patientportal_function/hrequest",
+    type:"post",
+    data: JSON.stringify({
+        "hospitalId": hid,
+        "hospitalPassword":hpass
+    }),
+    success: function(data){
+        alert(data.message);
+    },
+    error: function(error){
+        alert(error.message);
+    }
+   });
+}
--- functions/unified_patient_portal_function/index.js
+++ functions/unified_patient_portal_function/index.js
@@ -1,17 +1,96 @@
 'use strict';
+var express = require('express');
+var app = express();
+var catalyst = require('zcatalyst-sdk-node');
+app.use(express.json());
+var requestId=1;
+const tableName = 'request'; // The table created in the Data Store
+const columnName1 = 'hospitalName'; // The column created in the table
+const columnName2 = 'contactNumber';
+const columnName3 = 'mailId';
+const columnName4 = 'address'; // The column created in the table
 
-module.exports = (req, res) => {
-	var url = req.url;
+// The POST API that reports the alien encounter for a particular city
+app.post('/hrequest', (req, res) => {
+ var hospitalJson = req.body;
+// console.log(cityJson);
 
-	switch (url) {
-		case '/':
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.write('<h1>Hello from index.js<h1>');
-			break;
-		default:
-			res.writeHead(404);
-			res.write('You might find the page you are looking for at "/" path');
-			break;
-	}
-	res.end();
-};
+ // Initializing Catalyst SDK
+ var catalystApp = catalyst.initialize(req);
+   var rowData={};
+   rowData['requestId']=hospitalJson.requestId;
+   rowData[columnName1]=hospitalJson.hospitalName;
+   rowData[columnName2]=hospitalJson.contactNumber;
+   rowData[columnName3]=hospitalJson.mailId;
+   rowData[columnName4]=hospitalJson.address;
+   var rowArr=[];
+   rowArr.push(rowData);
+   requestId+=1;
+   // Inserts the city name as a row in the Catalyst Data Store table
+   catalystApp.datastore().table(tableName).insertRows(rowArr).then(requestInsertResp => {
+    res.send({
+     "message": "Request sent successfully!"
+    });
+   }).catch(err => {
+    console.log(err);
+    sendErrorResponse(res);
+   });
+}
+
+// The GET API that checks the table for an alien encounter in that city 
+
+
+
+
+app.get('/alien', (req, res) => {
+ var city = req.query.city_name;
+
+ // Initializing Catalyst SDK
+ var catalystApp = catalyst.initialize(req);
+
+ // Queries the Catalyst Data Store table and checks whether a row is present for the given city
+ getDataFromCatalystDataStore(catalystApp, city).then(cityDetails => {
+  if (cityDetails.length == 0) {
+   res.send({
+    "message": "Hurray! No alien encounters in this city yet!",
+    "signal": "negative"
+   });
+  } else {
+   res.send({
+    "message": "Uh oh! Looks like there are aliens in this city!",
+    "signal": "positive"
+   });
+  }
+ }).catch(err => {
+  console.log(err);
+  sendErrorResponse(res);
+ })
+});
+/**
+ * Checks whether an alien encounter is already reported for the given city by querying the Data Store table
+ * @param {*} catalystApp 
+ * @param {*} 
+ */
+function getDataFromCatalystDataStore(catalystApp, cityName) {
+ return new Promise((resolve, reject) => {
+  // Queries the Catalyst Data Store table
+  catalystApp.zcql().executeZCQLQuery("Select * from "+tableName+" where "+columnName+"='" + cityName + "'").then(queryResponse => {
+   resolve(queryResponse);
+  }).catch(err => {
+   reject(err);
+  })
+ });
+
+}
+
+/**
+ * Sends an error response
+ * @param {*} res 
+ */
+function sendErrorResponse(res) {
+ res.status(500);
+ res.send({
+  "error": "Internal server error occurred. Please try again in some time."
+ });
+}
+module.exports = app;
